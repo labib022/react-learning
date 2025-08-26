32921f737f88d8bfff2bd3250074a9f527f07336
Labib Hasan <labibshawon101@gmail.com>
2025-08-26 22:24:33 +0600

    new-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import Footer from './components/footer';
-// import Hero from './components/hero';
-import Home from './components/home/home';
-import Navbar from './components/footer';
-import ProfileCard from './components/profileCard';
-import Student from './components/student';
-import Arg from './components/Arg';
-import XI from './components/Argentina';
-import Counter from './components/about';
-import NameForm from './components/input';
-import LikeButton from './components/like';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div>
-      <Home />
-      {/* <Hero /> */}
+      {/* <Home /> */}
+
       <Navbar />
+
+      {/* <Hero /> */}
+      {/* {/* <Navbar /> 
       <Footer />
       <ProfileCard />
       <Student />
@@ -32,8 +25,8 @@ function App() {
         </h1>
         <Counter />
       </div>
-      <NameForm />
-      <LikeButton />
+      <NameForm /> */}
+      {/* <LikeButton /> */}
     </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,33 @@
 function Navbar() {
-  const name = 'Shawon';
-  const age = 22;
-  const city = 'Dhaka';
-
   return (
-    <div>
-      <h1>My name is {name}</h1>
-      <p>I am {age} years old</p>
-      <p>I lived in {city}</p>
+    <div className="bg-[#171010] py-5 flex items-center justify-between px-6">
+      <div className="flex gap-4 text-white x">
+        <a className="hover:border-b border-1 font-bold" href="">
+          Home
+        </a>
+        <a className="hover:border-b border-1" href="">
+          About
+        </a>
+        <a className="hover:border-b" href="">
+          Contact
+        </a>
+        <a className="hover:border-b" href="">
+          Portfolio
+        </a>
+        <a className="hover:border-b" href="">
+          Newsletter
+        </a>
+      </div>
+
+      <div className="flex gap-6 text-white">
+        <a className="hover:border-b" href="">
+          Sign In
+        </a>
+
+        <a className="hover:border-b" href="">
+          Sign Up
+        </a>
+      </div>
     </div>
   );
 }
